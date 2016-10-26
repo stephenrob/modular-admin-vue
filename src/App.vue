3f0cdc5566827f842e23c3d2e5f2be240f7b44da
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-10-26 16:12:04 +0100

    Added sidebar to modular admin demo app

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,40 @@
         </ModularAdminProfileDropdown>
       </ModularAdminHeaderNav>
     </ModularAdminAppHeader>
+
+    <ModularAdminSidebar>
+      <ModularAdminSidebarHeader appName="Modular Admin"></ModularAdminSidebarHeader>
+      <ModularAdminSidebarNav>
+
+        <ModularAdminSidebarNavItem
+            icon='fa-home'
+            link='#'
+            itemText='Dashboard'
+          >
+          </ModularAdminSidebarNavItem>
+          <ModularAdminSidebarNavDropdown
+            icon="fa-th-large"
+            itemText="Items Manager"
+          >
+            <ModularAdminSidebarNavItem
+              link='#itemslist'
+              itemText='Items List'
+            ></ModularAdminSidebarNavItem>
+            <ModularAdminSidebarNavItem
+              :active="true"
+              link='#itemseditor'
+              itemText='Item Editor'
+            ></ModularAdminSidebarNavItem>
+          </ModularAdminSidebarNavDropdown>
+          <ModularAdminSidebarNavItem
+            icon='fa-home'
+            link='#'
+            itemText='Testing'
+          ></ModularAdminSidebarNavItem>
+
+      </ModularAdminSidebarNav>
+      <ModularAdminSiderbarFooter></ModularAdminSiderbarFooter>
+    </ModularAdminSidebar>
     
   </ModularAdminApp>
 
@@ -51,6 +85,12 @@ import ModularAdminHeaderNav from './components/modular-admin/header/HeaderNav'
 import ModularAdminProfileDropdown from './components/modular-admin/header/profile-dropdown/ProfileDropdown'
 import ModularAdminDropdownItem from './components/modular-admin/dropdown/DropdownItem'
 import ModularAdminDropdownDivider from './components/modular-admin/dropdown/DropdownDivider'
+import ModularAdminSidebar from './components/modular-admin/sidebar/Sidebar'
+import ModularAdminSidebarHeader from './components/modular-admin/sidebar/SidebarHeader'
+import ModularAdminSidebarNav from './components/modular-admin/sidebar/nav/SidebarNav'
+import ModularAdminSidebarNavDropdown from './components/modular-admin/sidebar/nav/SidebarNavDropdown'
+import ModularAdminSidebarNavItem from './components/modular-admin/sidebar/nav/SidebarNavItem'
+import ModularAdminSidebarFooter from './components/modular-admin/sidebar/SidebarFooter'
 
 export default {
   name: 'app',
@@ -62,7 +102,13 @@ export default {
     ModularAdminHeaderNav,
     ModularAdminProfileDropdown,
     ModularAdminDropdownItem,
-    ModularAdminDropdownDivider
+    ModularAdminDropdownDivider,
+    ModularAdminSidebar,
+    ModularAdminSidebarHeader,
+    ModularAdminSidebarNav,
+    ModularAdminSidebarNavDropdown,
+    ModularAdminSidebarNavItem,
+    ModularAdminSidebarFooter
   }
 }
 </script>
