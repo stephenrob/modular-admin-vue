bd47209b80f4d2abcfa6c23cc686a0ae5720221b
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-10-26 14:49:16 +0100

    Add sidebar navigation components

---
+++ src/components/modular-admin/sidebar/nav/SidebarNav.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <nav class="menu">
+    <ul class="nav" id="sidebar-menu">
+      <slot></slot>
+    </ul>
+  </nav>
+
+</template>
+
+<script>
+  export default {
+    name: 'modular-admin-sidebar-nav'
+  }
+</script>
---
+++ src/components/modular-admin/sidebar/nav/SidebarNavDropdown.vue
@@ -0,0 +1,56 @@
+<template>
+
+<li :class="{active: isActive, open: show}">
+  <a href="#" @click="toggle($event)">
+    <i class="fa" :class="[icon]"></i>
+    {{ itemText }}
+    <i class="fa arrow"></i>
+  </a>
+  <ul>
+    <slot></slot>
+  </ul>
+</li>
+
+</template>
+
+<script>
+  export default {
+    name: 'modular-admin-sidebar-nav-dropdown',
+    replace: true,
+    data () {
+      return {
+        show: false,
+        isActive: false
+      }
+    },
+    props: {
+      active: {
+        type: Boolean,
+        default: false
+      },
+      icon: {
+        type: String,
+        default: ''
+      },
+      itemText: {
+        type: String,
+        default: ''
+      }
+    },
+    methods: {
+      toggle (e) {
+        this.show = !this.show
+        if (this.show) {
+          e.stopPropagation()
+        }
+      }
+    },
+    created: function () {
+      this.isActive = this.active
+      this.$on('sidebaritem::active', () => {
+        this.isActive = true
+        this.show = true
+      })
+    }
+  }
+</script>
