6b247ed9cffe22b39f028f0075b867fda4c70068
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-10-26 16:01:49 +0100

    Add basic header outline to demo app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,39 @@
     :fixedSidebar="true"
     :fixedFooter="true"
   >
+    <ModularAdminAppHeader>
+      <ModularAdminAppHeaderHideSidebar></ModularAdminAppHeaderHideSidebar>
+      <ModularAdminHeaderSearch></ModularAdminHeaderSearch>
+      <ModularAdminHeaderNav>
+        <ModularAdminProfileDropdown
+          name=" John Doe"
+          profilePictureURL="https://avatars3.githubusercontent.com/u/3959008?v=3&s=40"
+          :caret="true"
+        >
+          <ModularAdminDropdownItem
+            link="#dropdownItem1"
+            icon="fa-user"
+            text="Profile"
+          ></ModularAdminDropdownItem>
+          <ModularAdminDropdownItem
+            link="#dropdownItem2"
+            icon="fa-bell"
+            text="Notifications"
+          ></ModularAdminDropdownItem>
+          <ModularAdminDropdownItem
+            link="#dropdownItem3"
+            icon="fa-gear"
+            text="Settings"
+          ></ModularAdminDropdownItem>
+          <ModularAdminDropdownDivider></ModularAdminDropdownDivider>
+          <ModularAdminDropdownItem
+            link="#dropdownItem4"
+            icon="fa-power-off"
+            text="Logout"
+          ></ModularAdminDropdownItem>
+        </ModularAdminProfileDropdown>
+      </ModularAdminHeaderNav>
+    </ModularAdminAppHeader>
     
   </ModularAdminApp>
 
@@ -11,10 +44,25 @@
 
 <script>
 import ModularAdminApp from './components/modular-admin/App'
+import ModularAdminAppHeader from './components/modular-admin/header/AppHeader'
+import ModularAdminAppHeaderHideSidebar from './components/modular-admin/header/HeaderHideSidebar'
+import ModularAdminHeaderSearch from './components/modular-admin/header/HeaderSearch'
+import ModularAdminHeaderNav from './components/modular-admin/header/HeaderNav'
+import ModularAdminProfileDropdown from './components/modular-admin/header/profile-dropdown/ProfileDropdown'
+import ModularAdminDropdownItem from './components/modular-admin/dropdown/DropdownItem'
+import ModularAdminDropdownDivider from './components/modular-admin/dropdown/DropdownDivider'
+
 export default {
   name: 'app',
   components: {
-    ModularAdminApp
+    ModularAdminApp,
+    ModularAdminAppHeader,
+    ModularAdminHeaderSearch,
+    ModularAdminAppHeaderHideSidebar,
+    ModularAdminHeaderNav,
+    ModularAdminProfileDropdown,
+    ModularAdminDropdownItem,
+    ModularAdminDropdownDivider
   }
 }
 </script>
