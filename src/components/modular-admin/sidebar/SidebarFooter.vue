2ce5257423ba8dee66cb3c46e04edf363c587e4a
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-10-26 14:48:17 +0100

    Add sidebar header and footer components

---
+++ src/components/modular-admin/sidebar/SidebarFooter.vue
@@ -0,0 +1,13 @@
+<template>
+
+  <footer class="sidebar-footer">
+    <slot></slot>
+  </footer>
+
+</template>
+
+<script>
+  export default {
+    name: 'modular-admin-sidebar-footer'
+  }
+</script>
---
+++ src/components/modular-admin/sidebar/SidebarHeader.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div class="sidebar-header">
+    <div class="brand">
+      {{ appName }}
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'modular-admin-sidebar-header',
+    props: {
+      appName: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
