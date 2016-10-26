086e0b8aa8e0c1305baf4b4606145556909e37ca
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-10-26 14:48:05 +0100

    Add sidebar component

---
+++ src/components/modular-admin/sidebar/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <aside class="sidebar">
+    <div class="sidebar-container">
+      <slot></slot>
+    </div>
+  </aside>
+  
+</template>
+
+<script>
+  export default {
+    name: 'modular-admin-sidebar'
+  }
+</script>
